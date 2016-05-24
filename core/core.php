52018e6de0547435506b48b91a50a952778451bd
elboqueronpaco <elboqueronpaco@gmail.com>
2016-05-24 16:54:39 +0200

    Comenzando con Iniciar Session ajax y php

--- core/core.php
+++ core/core.php
@@ -6,9 +6,8 @@ EL NUCLEO DE LA APLICACION
 define('HTML_DIR', 'html/');
 define('CORE_DIR', 'core/');
 define('APP_TITLE', 'PamorciaProyectos');
+define('APP_URL', 'http://localhost/proyectos/proyectopamorcia/');
 
 require('vendor/autoload.php');
-
-
 
  ?>
--- html/overall/header.php
+++ html/overall/header.php
@@ -3,7 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title><?php echo APP_TITLE; ?></title>
+    <base href="<?php APP_URL; ?>"/>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="views/estilos/css/estilos.css">
     <link rel="stylesheet" href="views/estilos/css/iconos.css">
+    <script src="views/app/js/generales.js"></script>
   </head>
