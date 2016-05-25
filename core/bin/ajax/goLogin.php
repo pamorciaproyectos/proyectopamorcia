fff3d8e5267f4112240a21b7615c0f80a7ba3975
elboqueronpaco <elboqueronpaco@gmail.com>
2016-05-25 22:03:23 +0200

    goLogin.php

--- core/bin/ajax/goLogin.php
+++ core/bin/ajax/goLogin.php
@@ -1,7 +1,19 @@
 <?php
 
-$db = new Conexion();
-$datas = $db->real_escape_string($_POST['user']);
-$db->close();
+if (!empty($_POST['user']) and !empty($_POST['pass'])) {
+  $db = new Conexion();
+  $datas = $db->real_escape_string($_POST['user']);
+  $pass = Encrypt($_POST['pass']);
+  $sql = $db->query("SELECT id FROM users WHERE (user='$datas' OR email='$datas') AND pass='$pass' LIMIT 1;");
+  if ($db->rows($sql) > 0) {
+    echo 1;
+  }else {
+    echo '<strong>Error:</strong> Algún dato es incorrecto.';
+  }
+  $db->close();
+}else {
+  echo '<strong>Error:</strong> Todos los datos deben estar llenos';
+}
+
 
  ?>
